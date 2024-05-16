6d09cc273bac62e21ecac7dbeb2b826e1cdbf6f2
bluebird999280 <bluebird999280@gmail.com>
2024-05-16 13:05:00 +0900

    @chore react-router-dom 패키지 설치 및 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <div>인증 페이지</div>,
+	},
+]);
+
 function App() {
-	return <></>;
+	return (
+		<>
+			<RouterProvider router={router} />
+		</>
+	);
 }
 
 export default App;
