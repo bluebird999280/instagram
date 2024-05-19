8ecaf72e1aa47809d948fef231df22f977e96f8d
bluebird999280 <bluebird999280@gmail.com>
2024-05-19 09:35:23 +0900

    @add redux-persist 패키지 초기 세팅

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
-import user from "./slices/user/slice";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import user from "@/slices/user/slice";
 
-const store = configureStore({
-	reducer: { user },
-
-	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+const rootReducer = combineReducers({
+	user: user,
 });
 
-export default store;
+const persistConfig = {
+	key: "root",
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: false,
+		}),
+});
+
+export const persistor = persistStore(store);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
