2bf1ad7a9447516da49871d89ae6f5e7101b5e01
bluebird999280 <bluebird999280@gmail.com>
2024-05-28 08:06:45 +0900

    @add app.ts에 userRouter 추가

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import userRouter from "./routes/user";
 
 dotenv.config();
 const { PORT, MONGO_URI } = process.env;
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("files"));
 app.use(express.urlencoded({ extended: true }));
+app.use("/api/user", userRouter);
 
 async function main() {
 	try {
