42589f5146e020ae1d5721535b8654d9be7839af
bluebird999280 <bluebird999280@gmail.com>
2024-05-28 12:32:24 +0900

    @add postSchema에서 like, comment에 대한 필드 제거

--- src/models/post.ts
+++ src/models/post.ts
@@ -3,16 +3,12 @@ import { Schema } from "mongoose";
 interface IPostSchema {
 	author: string;
 	content?: string;
-	like: Schema.Types.ObjectId;
-	comment?: [Schema.Types.ObjectId];
 	createDate: Date;
 }
 
 const PostSchema = new Schema<IPostSchema>({
 	author: { type: String, required: true },
 	content: { type: [String], default: [] },
-	like: { type: Schema.Types.ObjectId, required: true },
-	comment: { type: [Schema.Types.ObjectId], default: [] },
 	createDate: { type: Date, default: Date.now },
 });
 
