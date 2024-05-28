bae63490a8e0816c8ce69d4ffa7a5f8857640dd2
bluebird999280 <bluebird999280@gmail.com>
2024-05-28 12:41:00 +0900

    @add accessToken을 관리하는 check 미들웨어 생성

---
+++ src/middlewares/check.ts
@@ -0,0 +1,58 @@
+import type { Request, Response, NextFunction } from "express";
+import { validateToken } from "../utils/jwt";
+import { JwtPayload } from "jsonwebtoken";
+
+declare global {
+	namespace Express {
+		interface Request {
+			user: JwtPayload;
+		}
+	}
+}
+
+/*
+ * 토큰 확인 api
+ * @headers authorization String
+ * status
+ * - [400] AccessToken is empty (token이 존재하지 않거나 잘못되었을 경우)
+ * - [401] AccessToken is expired. (token의 유효기간이 지났을 때)
+ * - [500] Unknown Error (알 수 없는 오류가 발생했을 때)
+ */
+
+type Error = {
+	errorName: string;
+	payload: undefined;
+};
+
+export default async function (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
+	if (
+		req.headers["authorization"] === undefined ||
+		req.headers["authorization"].split("Bearer").length !== 2
+	) {
+		return res.status(400).send({
+			message: "AccessToken is empty",
+		});
+	}
+
+	const accessToken = req.headers["authorization"].split("Bearer ")[1];
+
+	try {
+		const decodedAccessToken = await validateToken(accessToken);
+
+		if (decodedAccessToken.isExpired) {
+			return res.status(401).send("AccessToken is expired");
+		}
+
+		if (decodedAccessToken.token !== undefined) {
+			req.user = decodedAccessToken.token as JwtPayload;
+			return next();
+		}
+	} catch (error) {
+		console.error(error);
+		return res.status(500).send({ message: "Unknown error" });
+	}
+}
