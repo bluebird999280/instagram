0594751f48bebd1543f3d276ef2f4f0e7c7933a4
bluebird999280 <bluebird999280@gmail.com>
2024-05-28 09:19:34 +0900

    @add UserSchema에 phone, email, nickname에 인덱스 옵션 추가

--- src/models/user.ts
+++ src/models/user.ts
@@ -6,14 +6,22 @@ interface IUserSchema {
 	fullName: string;
 	nickName: string;
 	password: string;
+	photo: string;
+	follower: number;
+	following: number;
+	createDate: Date;
 }
 
 const UserSchema = new mongoose.Schema<IUserSchema>({
 	phone: {
 		type: String,
+		unique: true,
+		sparse: true,
 	},
 	email: {
 		type: String,
+		unique: true,
+		sparse: true,
 	},
 	fullName: {
 		type: String,
@@ -21,11 +29,29 @@ const UserSchema = new mongoose.Schema<IUserSchema>({
 	},
 	nickName: {
 		type: String,
+		index: true,
+		unique: true,
 		require: true,
 	},
 	password: {
 		type: String,
 		require: true,
+	},
+	photo: {
+		type: String,
+		default: "http://localhost:4000/images/defaultProfileImage.jpg",
+	},
+	follower: {
+		type: Number,
+		default: 0,
+	},
+	following: {
+		type: Number,
+		default: 0,
+	},
+	createDate: {
+		type: Date,
+		default: Date.now,
 	},
 });
 
