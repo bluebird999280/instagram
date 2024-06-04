80fa70f3c760756099a16aef2efc0d346b3c9e8c
bluebird999280 <bluebird999280@gmail.com>
2024-06-04 12:48:43 +0900

    @Feat 회원가입에서 발생하는 오류문 출력 문제 해결

--- src/utils/data/errors.ts
+++ src/utils/data/errors.ts
@@ -3,8 +3,10 @@ type errorsType = {
 };
 
 const errors: errorsType = {
-	"Input is invalid": "입력값이 존재하지 않거나 유효하지 않습니다.",
-	"User does not exist": "사용자가 존재하지 않습니다.",
+	"Input are invalid": "입력값이 존재하지 않거나 유효하지 않습니다.",
+	"Phone already exists": "휴대폰이 이미 존재합니다.",
+	"Email already exists": "이메일이 이미 존재합니다.",
+	"Nickname already exists": "닉네임이 이미 존재합니다.",
 	"Password is not correct": "비밀번호가 틀렸습니다.",
 	"Unable to communicate with the server": "서버에 접속할 수 없습니다.",
 	"Database unknown error": "서버에 장애가 발생했습니다.",
@@ -12,6 +14,8 @@ const errors: errorsType = {
 
 function errorMessage(error?: string) {
 	if (error === undefined) return false;
+	if (error !== undefined && !Object.keys(errors).includes(error))
+		return "알 수 없는 오류가 발생했습니다.";
 	return errors[error];
 }
 
