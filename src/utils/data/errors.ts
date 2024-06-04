60f3619c238c5b1c1a1ae92b0a0baa4bb5347e38
bluebird999280 <bluebird999280@gmail.com>
2024-06-05 03:37:16 +0900

    @Feat 로그인 에러메세지 수정

--- src/utils/data/errors.ts
+++ src/utils/data/errors.ts
@@ -7,6 +7,7 @@ const errors: errorsType = {
 	"Phone already exists": "휴대폰이 이미 존재합니다.",
 	"Email already exists": "이메일이 이미 존재합니다.",
 	"Nickname already exists": "닉네임이 이미 존재합니다.",
+	"User does not exist" : "해당 아이디가 존재하지 않습니다.",
 	"Password is not correct": "비밀번호가 틀렸습니다.",
 	"Unable to communicate with the server": "서버에 접속할 수 없습니다.",
 	"Database unknown error": "서버에 장애가 발생했습니다.",
